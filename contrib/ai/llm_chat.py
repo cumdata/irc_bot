312a36f923b79ec2b769fe2f4cc892a04111d565
ralff <90080949+deacon-ralph@users.noreply.github.com>
2024-12-08 21:58:01 -0500

    update

--- contrib/ai/llm_chat.py
+++ contrib/ai/llm_chat.py
@@ -55,7 +55,7 @@ Here is an example of the structure of chat_history: \n
 1733697209622 | play_games | yea shut up nerd lol\n
 <example>\n
 
-Here is the chat history which may contain between 0 to 100: \n
+Here is the chat history which may contain between 0 to 10 entries: \n
 
 <chat_history>
 {history}
